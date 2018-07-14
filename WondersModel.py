c01a14b7eff49fc0ec1808ba8200524158063249
LordRysiek <jakub.bilski@autograf.pl>
2018-07-14 18:42:12 +0200

    Changed sending information from string to enum. Thinking about algorithm

--- WondersModel.py
+++ WondersModel.py
@@ -3,6 +3,7 @@ class WondersModel:
         allPlacements = []
         self.possiblePlacements = []
         self.impossiblePlacements = []
+        self.chosenPlacements = []
         for wonder in wonderList:
             allPlacements.append((wonder.name, wonder.WhereCanItBeBuilt(fieldList, fieldListWithNearestBorders)))
         for placement in allPlacements:
@@ -13,13 +14,27 @@ class WondersModel:
         self.currentlyPossiblePlacements = self.possiblePlacements
 
     def GetCurrentlyPossibleWonders(self):
-        return [placement[0].name for placement in self.currentlyPossiblePlacements]
+        return [placement[0] for placement in self.currentlyPossiblePlacements]
 
     def GetCurrentlyImpossibleWonders(self):
-        return [placement[0].name for placement in self.possiblePlacements if placement not in self.currentlyPossiblePlacements]
+        return [placement[0] for placement in self.possiblePlacements if placement not in self.currentlyPossiblePlacements]
 
     def GetImpossibleWonders(self):
-        return [placement[0].name for placement in self.impossiblePlacements]
+        return [placement[0] for placement in self.impossiblePlacements]
 
     def ChooseWonder(self, wonderName):
-        
+        placement = self.FindPlacement(wonderName, self.possiblePlacements)
+        if not placement:
+            raise NameError('Tried to choose wonder that was not possible to choose')
+        self.chosenPlacements.append(placement)
+        self.currentlyPossiblePlacements.remove(placement)
+        self.UpdateCurrentlyPossiblePlacements()
+    #private
+    def FindPlacement(self, wonderName, list):
+        for x in list:
+            if x[0] == wonderName:
+                return x
+        return 0
+
+    def UpdateCurrentlyPossiblePlacements(self):
+        pass
