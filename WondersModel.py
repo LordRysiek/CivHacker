e3db70bc8663df7d5b03eb24c024b1dc57da875b
LordRysiek <jakub.bilski@autograf.pl>
2018-07-15 13:28:06 +0200

    Started an algorithm

--- WondersModel.py
+++ WondersModel.py
@@ -1,9 +1,15 @@
 class WondersModel:
+    """
+    using modified Ford-Fulkerson algorithm for biparite graphs
+    """
     def __init__(self, wonderList, fieldList, fieldListWithNearestBorders):
         allPlacements = []
         self.possiblePlacements = []
         self.impossiblePlacements = []
         self.chosenPlacements = []
+        self.FFCurrentMatching = [-1 for x in range(36)]
+        self.FFCurrentCombinations = []
+        self.FFCurrentCombinationsPossibleMatchingSets = []
         for wonder in wonderList:
             allPlacements.append((wonder.name, wonder.WhereCanItBeBuilt(fieldList, fieldListWithNearestBorders)))
         for placement in allPlacements:
@@ -28,7 +34,7 @@ class WondersModel:
             raise NameError('Tried to choose wonder that was not possible to choose')
         self.chosenPlacements.append(placement)
         self.currentlyPossiblePlacements.remove(placement)
-        self.UpdateCurrentlyPossiblePlacements()
+        self.UpdateCurrentlyPossiblePlacements(placement)
     #private
     def FindPlacement(self, wonderName, list):
         for x in list:
@@ -36,5 +42,60 @@ class WondersModel:
                 return x
         return 0
 
-    def UpdateCurrentlyPossiblePlacements(self):
-        pass
+    def UpdateCurrentlyPossiblePlacements(self, placement):
+        newCombinationIndex = -1
+        if len(placement) == 1:
+            self.FFCurrentCombinations.append([placement])
+            newCombinationIndex = len(self.FFCurrentCombinations)
+        elif len(placement) == 2:
+            for i in range(len(self.FFCurrentCombinations)):
+                if(self.FFCurrentCombinations[i][0][1][0] == placement[1][0]):
+                    self.FFCurrentMatching[:] = [x for x in self.FFCurrentMatching
+                                                            if x is not i]
+                    self.FFCurrentCombinations[i].append(placement)
+                    newCombinationIndex = i
+                    break
+            if newCombinationIndex == -1:
+                FFCurrentCombinations.append([placement])
+                newCombinationIndex = len(self.FFCurrentCombinations)
+        else:
+            raise NameError('Got a placement of zero or more than two length')
+        self.FFCurrentCombinationsPossibleMatchingSets[i] = self.FindPossibleMatchingSets(self.FFCurrentCombinations[i])
+
+    def FindPossibleMatchingSets(self, combination):
+        possibleDistrictFields = [fieldPair[1] for fieldPair in combination[0][1]]
+        for placement in combination:
+            districtFieldsToRemove = []
+            for pdf in range(len(possibleDistrictFields)):
+                for districtField in [fieldPair[1] for fieldPair in placement[1]]
+                    if districtField == pdf:
+                        break
+                districtFieldsToRemove.append(pdf)
+            possibleDistrictFields = [pdf for pdf in possibleDistrictFields if pdf not in districtFieldsToRemove]
+
+        possibleMatchingSets = []
+        for pdf in possibleDistrictFields:
+            combinationFieldsForPdf = []
+            for placement in combination:
+                combinationFieldsForPdf.append(placement[0], [fieldPair[0] for fieldPair in placement[1] if fieldPair[1] is pdf])
+            chosenFields = []
+            visitedFields = [0 for x in range(len(combination))]
+            i = 0
+            while True:
+                if i == len(combination):
+                    chosenFields.append(pdf)
+                    possibleMatchingSets.append(chosenFields)
+                    chosenFields.pop()
+                    chosenFields.pop()
+                    i = i-1
+                    continue
+                if len(combinationFieldsForPdf[i][1]) == visitedFields[i]:
+                    if i==0:
+                        break
+                    chosenFields.pop()
+                    i = i-1
+                    continue
+                chosenFields.append(combinationFieldsForPdf[i][1][visitedFields[i]])
+                visitedFields[i] = visitedFields[i] + 1
+                i = i+1
+        return possibleMatchingSets
